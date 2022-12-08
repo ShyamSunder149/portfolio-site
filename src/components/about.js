aea63bafd1e22573e7fc53d89beaa460be9899b3
Shyam Sunder Saravanan <SubstantialCitron0@protonmail.com>
2022-12-08 17:00:13 +0530

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,7 @@ const About = () => {
             
             <Heading>About</Heading>
             <br/>
-            A Curious guy who always love to explore and experiment things. Looking forward to work on a challenging role where I can learn a lot of stuffs and contribute to the betterment of the organisation.
+            A Curious guy who has passion for exploration and experimentation, and I love trying out new ideas and approaches in my work. In my career, I have focused on cybersecurity and development, and have gained a wealth of experience and knowledge in these areas. I am always looking for opportunities to learn and grow, and am excited to continue building my skills and expertise in cybersecurity and development. I enjoy working on challenging projects and am eager to take on new and exciting opportunities in these fields.
             
         </Container>
     );
